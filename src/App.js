c9861d199b1d11bed56af0ee01994ff07230057f
ss749236 <ss749236@gmail.com>
2023-07-09 17:03:33 +0530

    weather

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="weather">
+        <h1 className="heading">Weather App</h1>
+        <div className="inn">
+          <input type="text" className="form-control" />
+          <button className="btn" type="button">Search</button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
