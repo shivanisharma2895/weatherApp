9b33efb672126c5eecbab1357b45ae7026154a36
ss749236 <ss749236@gmail.com>
2023-08-08 16:49:35 +0530

    update

--- src/App.js
+++ src/App.js
@@ -40,18 +40,20 @@ function App() {
             onChange={handleChangeInput} />
           <button className="btn btn-primary" type="button" onClick={handleSearch}>Search</button>
         </div>
-      </div>
 
-      {Object.keys(data).length > 0 &&
-        <div className="col-md-12 text-center mt-5">
-          <div className="shadow rounded weatherResultBox">
-            <h5 className="weatherCity">{data?.name}</h5>
-            <h6 className="weatherTemp">{((data?.main?.temp) - 273.15).toFixed(2)}℃</h6>
+
+        {Object.keys(data).length > 0 &&
+          <div className="result">
+            <div className="weather-result-box">
+              <h4 className="weatherCity">{data?.name}</h4>
+              <h5 className="weatherTemp">{((data?.main?.temp) - 273.15).toFixed(2)}℃</h5>
+              <h6>Max_temp: {((data?.main?.temp_max) - 273.15).toFixed(2)}℃ || Min_temp:{((data?.main?.temp_min) - 273.15).toFixed(2)}℃</h6>
+              <h6>Wind_speed: {data?.wind.speed}|| Humidity: {data?.main.humidity}</h6>
+            </div>
           </div>
-        </div>
-      }
+        }
 
-
+      </div>
     </div>
   );
 }
